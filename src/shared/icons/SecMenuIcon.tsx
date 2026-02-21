0f241b7368936ecb4afd0412488ec77da6476c22
katy-paola <kbarbozab@unicartagena.edu.co>
2026-02-21 16:53:33 -0500

    feat: add new logos and posters for characters

---
+++ src/shared/icons/SecMenuIcon.tsx
@@ -0,0 +1,21 @@
+import { SVG_ATTRIBUTES } from "../consts";
+import type { IconProps } from "../types";
+
+export default function SecMenuIcon(props: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={props.className ?? SVG_ATTRIBUTES.size}
+      viewBox="0 0 16 16"
+      fill="none"
+    >
+      <path
+        d="M2.66666 3.33331H13.3333M2.66666 7.99998H13.3333M2.66666 12.6666H13.3333"
+        stroke={SVG_ATTRIBUTES.color}
+        strokeWidth={SVG_ATTRIBUTES.strokeWidth}
+        strokeLinecap={SVG_ATTRIBUTES.strokeLinecap}
+        strokeLinejoin={SVG_ATTRIBUTES.strokeLinejoin}
+      />
+    </svg>
+  );
+}
