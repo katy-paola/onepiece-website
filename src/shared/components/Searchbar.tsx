2e56c401b7872c937cb3aa62d6c1e943b79d7313
katy-paola <kbarbozab@unicartagena.edu.co>
2026-03-10 21:43:43 -0500

    styles (searchbar): apply layout and colors

--- src/shared/components/Searchbar.tsx
+++ src/shared/components/Searchbar.tsx
@@ -14,14 +14,15 @@ export default function Searchbar({
   inputAttributes,
 }: SearchBarProps) {
   return (
-    <label>
+    <label className="flex items-center gap-2 px-5 py-4 border border-action-text/70 focus-within:border-action-text transition-colors">
+      <SearchIcon className="text-action-text" />
       <Input
+        className="w-full outline-none text-action-text/80 placeholder:text-action-text/80"
         type="search"
         placeholder={placeholder}
         value={inputAttributes.value}
         onChange={(e) => inputAttributes.onChange(e.target.value)}
       />
-      <SearchIcon />
     </label>
   );
 }
