aa4149096135a974593836df3a4a0cfad6eb4641
katy-paola <kbarbozab@unicartagena.edu.co>
2026-03-10 22:37:32 -0500

    feat: improve Header component with responsive navigation and state management

--- src/shared/ui/Header.tsx
+++ src/shared/ui/Header.tsx
@@ -3,25 +3,50 @@ import { NAVBAR_LINKS } from "../consts";
 import Button from "../components/Button";
 import MainMenuIcon from "../icons/MainMenuIcon";
 import CancelIcon from "../icons/CancelIcon";
+import { useState } from "react";
+import { cn } from "../lib/utils";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
-    <header>
+    <header className="relative flex justify-between items-center p-4 bg-radial z-10">
       <Link to="/" title="Go to homepage">
-        <img src="/logos/onepiece-logo.png" alt="One Piece Logo" />
+        <img
+          src="/logos/onepiece-logo.png"
+          alt="One Piece Logo"
+          className="h-10"
+        />
       </Link>
-      <nav>
-        <ul>
+      <nav
+        className={cn(
+          "flex justify-between items-start p-4 absolute top-0 z-10 bg-radial w-full left-full transition-transform duration-300",
+          isOpen && "-translate-x-full",
+        )}
+      >
+        <ul className="flex-1">
           {NAVBAR_LINKS.map((link, index) => (
-            <li key={index}>
-              <Link to={link.href}>{link.label}</Link>
+            <li key={index} className="flex" onClick={() => setIsOpen(false)}>
+              <Link className="p-2" to={link.href}>
+                {link.label}
+              </Link>
             </li>
           ))}
         </ul>
+        <Button
+          className=""
+          onClick={() => setIsOpen(false)}
+          variant="icon"
+          hierarchy="secondary"
+        >
+          <CancelIcon />
+        </Button>
       </nav>
-      <Button>
+      <Button
+        onClick={() => setIsOpen(true)}
+        variant="icon"
+        hierarchy="secondary"
+      >
         <MainMenuIcon />
-        <CancelIcon />
       </Button>
     </header>
   );
