4e7ee16cf9bfacd9df90637deca8f9bd826f9409
katy-paola <kbarbozab@unicartagena.edu.co>
2026-03-13 22:03:59 -0500

    ui (fruits): add styles to FruitList component

--- src/features/fruits/components/FruitsList.tsx
+++ src/features/fruits/components/FruitsList.tsx
@@ -2,26 +2,27 @@ import type { Fruit } from "../types";
 
 export default function FruitsList({ fruits }: { fruits: Fruit[] }) {
   return (
-    <ul>
-      {
-        /** Here we will put all the fruits from api */
-        fruits.map((fruit, index) => (
-          <li key={index}>
-            <button>
+    <ul className="grid gap-4 text-action-text md:grid-cols-2">
+      {fruits.map((fruit) => (
+        <li key={fruit.id} title={`${fruit.roman_name} / ${fruit.name}`}>
+          <button className="flex items-center gap-2 p-2 w-full bg-radial border border-stroke/30 shadow-card hover:shadow-none transition-all">
+            <picture className="h-16 aspect-square">
               <img
                 src={fruit.filename}
                 alt={`${fruit.roman_name} / ${fruit.name}`}
+                loading="lazy"
+                className="size-full object-contain"
               />
-              <span>
-                <span>
-                  {fruit.roman_name} / {fruit.name}
-                </span>
-                <small>{fruit.type}</small>
+            </picture>
+            <span className="flex flex-col gap-2 text-start">
+              <span className="font-medium line-clamp-1">
+                {fruit.roman_name} / {fruit.name}
               </span>
-            </button>
-          </li>
-        ))
-      }
+              <small className="text-xs">{fruit.type}</small>
+            </span>
+          </button>
+        </li>
+      ))}
     </ul>
   );
 }
