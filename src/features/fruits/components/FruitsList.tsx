fa0054afb0da579e3e91959ca64b037da9b21625
katy-paola <kbarbozab@unicartagena.edu.co>
2026-03-13 22:27:58 -0500

    ui (fruits): add styles to FruitModal component and handle visibility

--- src/features/fruits/components/FruitsList.tsx
+++ src/features/fruits/components/FruitsList.tsx
@@ -1,28 +1,55 @@
+import { useEffect, useRef, useState } from "react";
 import type { Fruit } from "../types";
+import FruitModal from "./FruitModal";
 
 export default function FruitsList({ fruits }: { fruits: Fruit[] }) {
+  const [selectedFruit, setSelectedFruit] = useState<Fruit | null>(null);
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    if (selectedFruit) {
+      dialogRef.current?.showModal();
+    }
+  }, [selectedFruit]);
   return (
-    <ul className="grid gap-4 text-action-text md:grid-cols-2">
-      {fruits.map((fruit) => (
-        <li key={fruit.id} title={`${fruit.roman_name} / ${fruit.name}`}>
-          <button className="flex items-center gap-2 p-2 w-full bg-radial border border-stroke/30 shadow-card hover:shadow-none transition-all">
-            <picture className="h-16 aspect-square">
-              <img
-                src={fruit.filename}
-                alt={`${fruit.roman_name} / ${fruit.name}`}
-                loading="lazy"
-                className="size-full object-contain"
-              />
-            </picture>
-            <span className="flex flex-col gap-2 text-start">
-              <span className="font-medium line-clamp-1">
-                {fruit.roman_name} / {fruit.name}
+    <>
+      <ul className="grid gap-4 text-action-text md:grid-cols-2">
+        {fruits.map((fruit) => (
+          <li key={fruit.id} title={`${fruit.roman_name} / ${fruit.name}`}>
+            <button
+              onClick={() => setSelectedFruit(fruit)}
+              className="flex items-center gap-2 p-2 w-full bg-radial border border-stroke/30 shadow-card hover:shadow-none transition-all"
+            >
+              <picture className="h-16 aspect-square">
+                <img
+                  src={fruit.filename}
+                  alt={`${fruit.roman_name} / ${fruit.name}`}
+                  loading="lazy"
+                  className="size-full object-contain"
+                />
+              </picture>
+              <span className="flex flex-col gap-2 text-start">
+                <span className="font-medium line-clamp-1">
+                  {fruit.roman_name} / {fruit.name}
+                </span>
+                <small className="text-xs">{fruit.type}</small>
               </span>
-              <small className="text-xs">{fruit.type}</small>
-            </span>
-          </button>
-        </li>
-      ))}
-    </ul>
+            </button>
+          </li>
+        ))}
+      </ul>
+      {selectedFruit && (
+        <dialog
+          ref={dialogRef}
+          onClose={() => setSelectedFruit(null)}
+          className="fixed inset-0 m-auto max-w-80 max-h-[80vh] md:max-w-xl"
+        >
+          <FruitModal
+            fruit={selectedFruit}
+            setSelectedFruit={setSelectedFruit}
+          />
+        </dialog>
+      )}
+    </>
   );
 }
