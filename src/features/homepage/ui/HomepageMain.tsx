b0d4e1924ef711d525baee8d275d1b337fa1b3c4
katy-paola <kbarbozab@unicartagena.edu.co>
2026-03-14 21:21:02 -0500

    ui (homepage): add styles to call to action

--- src/features/homepage/ui/HomepageMain.tsx
+++ src/features/homepage/ui/HomepageMain.tsx
@@ -7,7 +7,12 @@ export default function HomepageMain() {
     <>
       <Hero currentHero="homepage" />
       <ThemeList />
-      <LinkElement href="/episodes">Get Started</LinkElement>
+      <LinkElement
+        href="/episodes"
+        className="fixed left-4 right-4 bottom-4 text-center md:static md:w-max md:mx-auto"
+      >
+        Get Started
+      </LinkElement>
     </>
   );
 }
